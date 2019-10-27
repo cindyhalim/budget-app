df0df8ed2e957363ca07110549455e6e83afe092
Cindy Halim <jcindyhalim@gmail.com>
2019-10-27 16:15:58 -0400

    Add controllers for login and register

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
-  # before_filter headers['Access-Control-Allow-Origin'] = '*'  
+  # before_filter headers['Access-Control-Allow-Origin'] = '*' 
+
+  #ignore csrf token to communicate with router which is used when user is inputing in rails form, not needed for our app
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token 
 end
