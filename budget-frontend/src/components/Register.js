1253de1bc5d032d5c71d29657499464082567d07
Cindy Halim <jcindyhalim@gmail.com>
2019-10-29 18:44:40 -0400

    finish new goal component and add states pair program with nikita

--- budget-frontend/src/components/Register.js
+++ budget-frontend/src/components/Register.js
@@ -38,13 +38,11 @@ export default function Register(props) {
       )
       .then(res => {
         props.handleLogin(res);
-        const userID = res.data.user[0].id;
         axios
           .post(
             "http://localhost:3000/goals",
             {
               goal: {
-                user_id: userID,
                 goal_type: "budget",
                 amount: parseInt(budget),
                 name: "budget",
