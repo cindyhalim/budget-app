765cf9e498d4cee3694e18e2a23466ae85bdbb1d
ahana <ahanaghosh94@gmail.com>
2019-10-28 20:44:46 -0400

    add filter option for bar, pie progrees bar

--- budget-frontend/src/App.js
+++ budget-frontend/src/App.js
@@ -4,32 +4,27 @@ import Highcharts from "highcharts";
 import "./App.css";
 import axios from "axios";
 
-import { Progress } from "semantic-ui-react";
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
 function App() {
   const [state, setState] = useState("test");
   const [total, setTotal] = useState(0);
   const [goal, setGoal] = useState(0);
-  const [originalData, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [currentExpenseByCategory, setCurrentBar] = useState({});
+  const [selectedMonth, setMonth] = useState("September");
+  const [filteredMonth, setFilter] = useState({});
 
   useEffect(() => {
     axios.get("http://localhost:3000/ping").then(res => {
-      let categories = getCategoryTotal(res.data.transactions);
-      let totalMonthExpense = getTotal(categories);
-      let pieChart = chartDataPoints(categories, totalMonthExpense);
-      console.log(
-        new Date(res.data.transactions[0].transaction_date).toLocaleString(
-          "default",
-          { month: "long" }
-        )
-      );
-      setState(pieChart);
-      setData(res.data.transactions);
-      setTotal(totalMonthExpense);
+      setOriginalData(res.data.transactions);
       setGoal(parseInt(res.data.goals["0"].amount));
     });
   }, []);
+
+  useEffect(() => {
+    if (originalData.length !== 0) {
+      getChartbyMonth(new Date().toLocaleString("default", { month: "long" }));
+    }
+  }, [originalData]);
 
   useEffect(() => {
     Highcharts.chart({
@@ -140,7 +135,84 @@ function App() {
         }
       ]
     });
-  });
+  }, [total, goal]);
+
+  useEffect(() => {
+    let xAxisCategories = [
+      selectedMonth,
+      new Date().toLocaleString("default", { month: "long" })
+    ];
+    // console.log(filteredMonth);
+    let foodAndDrink = [
+      filteredMonth["Food and Drink"],
+      currentExpenseByCategory["Food and Drink"]
+    ];
+    let rideshare = [
+      filteredMonth["Rideshare"],
+      currentExpenseByCategory["Rideshare"]
+    ];
+    let shopping = [
+      filteredMonth["Shopping"],
+      currentExpenseByCategory["Shopping"]
+    ];
+    let recreation = [
+      filteredMonth["Recreation"],
+      currentExpenseByCategory["Recreation"]
+    ];
+
+    Highcharts.chart({
+      chart: {
+        type: "column",
+        renderTo: "bar-expenses-graph"
+      },
+      title: {
+        text: "This year"
+      },
+      xAxis: {
+        categories: xAxisCategories,
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Expenses (CAD)"
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat:
+          '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: "</table>",
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [
+        {
+          name: "Food and Drink",
+          data: foodAndDrink
+        },
+        {
+          name: "Rideshare",
+          data: rideshare
+        },
+        {
+          name: "Recreation",
+          data: recreation
+        },
+        {
+          name: "Shopping",
+          data: shopping
+        }
+      ]
+    });
+  }, [currentExpenseByCategory]);
 
   function getCategoryTotal(transactions) {
     let categories = {};
@@ -173,6 +245,7 @@ function App() {
   }
   function getChartbyMonth(month) {
     console.log("month:", month);
+    console.log("originaldata:", originalData);
     let filteredTransactions = originalData.filter(
       transaction =>
         new Date(transaction.transaction_date).toLocaleString("default", {
@@ -180,30 +253,61 @@ function App() {
         }) === month
     );
 
-    console.log(filteredTransactions);
     let categories = getCategoryTotal(filteredTransactions);
     let totalMonthExpense = getTotal(categories);
     let pieChart = chartDataPoints(categories, totalMonthExpense);
     setState(pieChart);
     setTotal(totalMonthExpense);
+    setMonth(month);
+
+    setCurrentBar(categories);
+    setFilter(getCategoryTotal(filteredTransactions));
+    return categories;
   }
+
+  // function getCurrentChartbyMonth(month) {
+  //   console.log("month:", month);
+  //   console.log("originaldata:", originalData);
+  //   let filteredTransactions = originalData.filter(
+  //     transaction =>
+  //       new Date(transaction.transaction_date).toLocaleString("default", {
+  //         month: "long"
+  //       }) === month
+  //   );
+
+  //   let categories = getCategoryTotal(filteredTransactions);
+
+  //   return categories;
+  // }
 
   return (
     <div className="App">
       <div>
         <h1>My Expenses </h1>
         <div id="progress-bar"></div>
+        <select
+          name=""
+          id="select"
+          onChange={e => {
+            getChartbyMonth(e.target.value);
+          }}
+        >
+          <option value="January">January</option>
+          <option value="February">February</option>
+          <option value="March">March</option>
+          <option value="April">April</option>
+          <option value="May">May</option>
+          <option value="June">June</option>
+          <option value="July">July</option>
+          <option value="August">August</option>
+          <option value="September">September</option>
+          <option value="October">October</option>
+          <option value="November">November</option>
+          <option value="December">December</option>
+        </select>
         <div id="Expenses-graph"></div>
-        {/* <CanvasJSChart options={options} /> */}
+        <div id="bar-expenses-graph"></div>
       </div>
-      <select
-        name=""
-        id="select"
-        onChange={e => getChartbyMonth(e.target.value)}
-      >
-        <option value="September">September</option>
-        <option value="October">October</option>
-      </select>
     </div>
   );
 }
