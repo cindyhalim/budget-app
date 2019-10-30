be77bc1235de7ad882e7cf1f252a64877627dbab
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-10-30 15:40:47 -0400

    add navbar to all pages, and add input to transaction page

--- budget-frontend/src/components/Profile.js
+++ budget-frontend/src/components/Profile.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Profile() {
-  return <div>PROFILE</div>;
+  return (
+    <div>
+      PROFILE<Navbar></Navbar>
+    </div>
+  );
 }
