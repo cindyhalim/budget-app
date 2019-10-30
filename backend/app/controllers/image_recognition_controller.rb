970e90914be47ef491d2ab3e7d0ada3c9327b2f3
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-10-30 13:29:27 -0400

    add google vision api and carrierwave uploader to the backend file

--- backend/app/controllers/image_recognition_controller.rb
+++ backend/app/controllers/image_recognition_controller.rb
@@ -1,5 +1,15 @@
 class ImageRecognitionController < ApplicationController
     def create
-        
+    require "google/cloud/vision"
+    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    @uploader = PicUploader.new
+    pp params[:image]
+    # @uploader.store!(params[:image])
+    # redirect_to "http://localhost:5000/picture/?file=#{@uploader.identifier}"
+    # image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    # file_name = "/Users/nikita/budget-app/backend/public/uploads/#{params[:id]}.jpg"
+    # response = image_annotator.text_detection image: file_name
+    # render json: {result: response.to_json}
+    # send_data("/Users/nikita/budget-app/backend/public/uploads/#{params[:id]}.jpg",:type => 'image/jpeg')
     end
 end
