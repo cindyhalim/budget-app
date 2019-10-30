0a3be73a8359114f7b92d0b4823580849d528456
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-10-30 13:50:16 -0400

    add carrierwave, and google/vision to gem file, upload file submitted through front end to back end local directory

--- backend/app/controllers/image_recognition_controller.rb
+++ backend/app/controllers/image_recognition_controller.rb
@@ -1,14 +1,13 @@
 class ImageRecognitionController < ApplicationController
     def create
-    require "google/cloud/vision"
-    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-    @uploader = PicUploader.new
+    # require "google/cloud/vision"
     pp params[:image]
-    # @uploader.store!(params[:image])
-    # redirect_to "http://localhost:5000/picture/?file=#{@uploader.identifier}"
-    # image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-    # file_name = "/Users/nikita/budget-app/backend/public/uploads/#{params[:id]}.jpg"
-    # response = image_annotator.text_detection image: file_name
+    @uploader = PicUploader.new
+    @uploader.store!(params[:image])
+    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    file_name = "/Users/nikita/budget-app/backend/public/uploads/#{params[:id]}.jpg"
+    response = image_annotator.text_detection image: file_name
+    pp response
     # render json: {result: response.to_json}
     # send_data("/Users/nikita/budget-app/backend/public/uploads/#{params[:id]}.jpg",:type => 'image/jpeg')
     end
