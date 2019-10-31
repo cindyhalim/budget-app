a3e86bc9405808a812703c500b78c0e543eb7331
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-10-30 20:58:15 -0400

    move new transaction manual entry dialog directly into the return section  of NavBar to fix minor bug, delete NewTransaction component, and  delete route pointing to the NewTransaction component

--- backend/app/controllers/goals_controller.rb
+++ backend/app/controllers/goals_controller.rb
@@ -1,5 +1,6 @@
 class GoalsController < ApplicationController
   def create
+    pp "In HERE"
     user = User.find_by(id: params['goal']['user_id'])
     pp user
     goal = user.goals.create(end_date: params['goal']['end_date'], user_id: params['goal']['user_id'], goal_type: params['goal']['goal_type'], amount: params['goal']['amount'], name: params['goal']['name'])
