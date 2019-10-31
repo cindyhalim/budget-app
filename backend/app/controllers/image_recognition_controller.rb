46cdc4570b163daf522899d5302efb13bd41f482
ahana <ahanaghosh94@gmail.com>
2019-10-31 11:12:17 -0400

    fix filepath issue in image_recognition controller

--- backend/app/controllers/image_recognition_controller.rb
+++ backend/app/controllers/image_recognition_controller.rb
@@ -5,7 +5,8 @@ class ImageRecognitionController < ApplicationController
     @uploader = PicUploader.new
     @uploader.store!(params[:image])
     image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-    file_name = "/Users/nikita/budget-app/backend/public/uploads/#{params[:image].original_filename}"
+    file_name = "./public/uploads/#{params[:image].original_filename}"
+    pp file_name
     @response = image_annotator.text_detection image: file_name
     render json: @response
     # pp response
