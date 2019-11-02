d81e17e1dbb155ae47976b5037e75469e068965b
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-11-02 17:11:06 -0400

    add date_last_checked as column to table users

--- backend/db/migrate/20191101235336_add_hp_to_users.rb
+++ backend/db/migrate/20191101235336_add_hp_to_users.rb
@@ -2,5 +2,6 @@ class AddHpToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :hp, :integer, :default => 100
     add_column :users, :coins, :integer, :default => 20
+    add_column :users, :date_last_checked, :date, :default => "2000-01-01"
   end
 end
