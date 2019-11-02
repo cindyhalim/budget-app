b840f2b4ecf4bed5da4e81be666a4e8f5ce71421
ahana <ahanaghosh94@gmail.com>
2019-11-02 13:06:55 -0400

    create monthly progress bar in analytics page

--- budget-frontend/src/components/Analytics.js
+++ budget-frontend/src/components/Analytics.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Piechart from "./Piechart";
 import Barchart from "./Barchart";
-
+import MonthlyProgressBar from "./MonthlyProgressBar";
 import Navbar from "./Navbar";
 
 const chartDropdown = {
@@ -13,6 +13,7 @@ export default function Analytics() {
   const [chart, setChart] = useState("Current Breakdown");
   return (
     <div>
+      <MonthlyProgressBar />
       <select
         name=""
         id="select"
