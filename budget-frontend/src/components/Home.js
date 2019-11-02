29a1ea94817e83a8e18f8f0242a89e9044395814
Cindy Halim <jcindyhalim@gmail.com>
2019-11-02 13:42:14 -0400

    Style first page

--- budget-frontend/src/components/Home.js
+++ budget-frontend/src/components/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import "../styles/Home.sass";
 
 export default function Home(props) {
   return (
-    <div>
-      <h1>Budgie</h1>
-      <Link to="/login">Login</Link>
-
-      <Link to="/register">Register</Link>
+    <div className="Home">
+      <h1 className="title">Budgie</h1>
+      <img src="pig.png" className="logo"></img>
+      <Button variant="contained" color="primary" className="button">
+        <Link to="/login">Login</Link>
+      </Button>
+      <Button variant="contained" color="" className="button">
+        <Link to="/register">Register</Link>
+      </Button>
     </div>
   );
 }
