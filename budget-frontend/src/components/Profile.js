a6f62f719054db46c927ae7bb0df026477b085f8
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-11-03 17:28:27 -0500

    create sass page for profile page, add styling

--- budget-frontend/src/components/Profile.js
+++ budget-frontend/src/components/Profile.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import { datePickerDefaultProps } from "@material-ui/pickers/constants/prop-types";
 import EditProfile from "./EditProfile";
 import EditBudget from "./EditBudget";
+import "../styles/ProfilePage.sass";
 
 export default function Profile(props) {
   const history = useHistory();
@@ -13,27 +14,51 @@ export default function Profile(props) {
   props.checkLogInStatus();
   return (
     <div>
-      <div style={{ height: "50px", width: "20%", border: "1px solid black" }}>
-        Profile Pic
+      <div className="profile-page">
+        <div className="header">
+          <img src="pig.png" className="avatar"></img>
+          <div className="header-profile">
+            {/* <p className="username">Hi {props.logInStatus.user.name}</p> */}
+            <p className="profile-text">Profile Page</p>
+            <button
+              className="logout"
+              onClick={() => {
+                props.logOutClick();
+                history.push("/");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+        <div className="description">
+          <p>
+            Edit your profile information or change your monthly budget goal
+            below. <strong>Happy Savings!</strong>
+          </p>
+        </div>
+        <div className="edit-fields">
+          <button
+            className="edit-user-fields-button"
+            onClick={() => setOpenEdit(!openEdit)}
+          >
+            Edit Profile
+          </button>
+          {openEdit ? (
+            <EditProfile closeEditProfile={() => setOpenEdit(false)} />
+          ) : null}
+          <button
+            className="edit-budget-fields-button"
+            onClick={() => setOpenBudget(!openBudget)}
+          >
+            Edit Budget
+          </button>
+          {openBudget ? (
+            <EditBudget closeEditBudget={() => setOpenBudget(false)} />
+          ) : null}
+        </div>
       </div>
-      <p>Username {props.logInStatus.user.name}</p>
-      <p
-        onClick={() => {
-          props.logOutClick();
-          history.push("/");
-        }}
-      >
-        Logout
-      </p>
       <Navbar />
-      <button onClick={() => setOpenEdit(!openEdit)}>Edit Profile</button>
-      <button onClick={() => setOpenBudget(!openBudget)}>Edit Budget</button>
-      {openEdit ? (
-        <EditProfile closeEditProfile={() => setOpenEdit(false)} />
-      ) : null}
-      {openBudget ? (
-        <EditBudget closeEditBudget={() => setOpenBudget(false)} />
-      ) : null}
     </div>
   );
 }
