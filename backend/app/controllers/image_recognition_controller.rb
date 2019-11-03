9c84f584c16e72f033467cc7b22d2addddc8cdb5
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-11-03 15:53:15 -0500

    figure out logic to get the amount from mc donalds receipts

--- backend/app/controllers/image_recognition_controller.rb
+++ backend/app/controllers/image_recognition_controller.rb
@@ -1,13 +1,17 @@
 class ImageRecognitionController < ApplicationController
     def create
-    # require "google/cloud/vision"
+
+    require "pry"
     pp params[:image]
     @uploader = PicUploader.new
     @uploader.store!(params[:image])
     image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    pp params[:image].original_filename
     file_name = "./public/uploads/#{params[:image].original_filename}"
+    pp "FILE NAME"
     pp file_name
     @response = image_annotator.text_detection image: file_name
+    # pp @response.full_text_annotation
     render json: @response
     # pp response
     # render json: {result: response.to_json}
