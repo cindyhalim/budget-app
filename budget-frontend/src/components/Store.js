fd57ba64ed86e6e39c644202e2fc8a652989b20a
Cindy Halim <jcindyhalim@gmail.com>
2019-11-04 18:26:35 -0500

    Style store page

--- budget-frontend/src/components/Store.js
+++ budget-frontend/src/components/Store.js
@@ -3,88 +3,112 @@ import Navbar from "./Navbar";
 import Badges from "./Badges";
 import { useHistory } from "react-router-dom";
 
+import { Card, CardContent } from "@material-ui/core";
 import StoreMallDirectoryIcon from "@material-ui/icons/StoreMallDirectory";
 
 import "../styles/Store.sass";
 
 export default function Store(props) {
   const history = useHistory();
-  console.log(history.location.pathname);
+
   props.checkLogInStatus();
   return (
-    <div>
-      <div className="store-title">
+    <div className="store-page">
+      <h1 className="store-title">
         <StoreMallDirectoryIcon className="store-icon" />
-        <div>Store</div>
+        Store
+      </h1>
+      <div className="budget-status">
+        You have met your budget <strong>{props.budgetAchieved}</strong> times
+        this year
       </div>
-
-      <div>
-        <div className="entire-store">
-          <div className="status-card">
-            You have met your budget <strong>{props.budgetAchieved}</strong>{" "}
-            times this year
+      <h3 className="store-headings">Inventory</h3>
+      <Card className="store-content inventory">
+        <CardContent className="store-items">
+          <span>
+            <p className="item-title">
+              {" "}
+              <img
+                className="item-img"
+                src="coins.jpg"
+                style={{ height: "50px", width: "50px" }}
+              />
+              Coins
+            </p>
+          </span>
+          <p className="amount">{props.coins}</p>
+        </CardContent>
+      </Card>
+      <h3 className="store-headings">Market</h3>
+      <Card className="store-content market">
+        <CardContent className="store-items">
+          <div>
+            <img
+              src="potion.png"
+              className="item-img"
+              style={{ height: "60px", width: "60px" }}
+            />
+            <section>
+              <p className="item-title">Potion</p>
+              <p className="item-description">+ 20 HP</p>
+            </section>
           </div>
-
-          <div className="inventory-card">
-            <div className="inventory-title">Inventory</div>
-            <div className="coins-inventory">
-              <div className="coin-title">Coins:</div>
-              <div className="coin-count">
-                <div>{props.coins}</div>
-                <div>
-                  <img
-                    src="coins.jpg"
-                    style={{ height: "30px", width: "30px" }}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="market-card">
-            <div className="market-title">Market</div>
-            <div className="potion-market">
-              <div className="potion-details">
-                <div>Potion</div>
-                <img
-                  src="potion.png"
-                  style={{ height: "30px", width: "30px" }}
-                />
-                <div>Heals 20 HP</div>
-              </div>
-              <div>
-                <div>
-                  20
-                  <img
-                    src="coins.jpg"
-                    style={{ height: "30px", width: "30px" }}
-                  />
-                </div>
-                <button
-                  onClick={() => {
-                    if (props.hp !== 100 && props.coins >= 20) {
-                      props.subtractCoinsAddHP(-20, 20);
-                    } else if (props.coins < 20) {
-                      alert("You don't have enough money");
-                    } else {
-                      alert("Your health is already full");
-                    }
-                  }}
-                  className="buy-button"
-                >
-                  Buy
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="market-title">My Badges</div>
-          <div className="badge-collection-card">
-            {Array.from(props.images).length > 0 &&
-              Array.from(props.images).map(image => {
-                return <Badges image={image} />;
-              })}
-          </div>
-        </div>
+          <button
+            onClick={() => {
+              if (props.hp !== 100 && props.coins >= 20) {
+                props.subtractCoinsAddHP(-20, 20);
+              } else if (props.coins < 20) {
+                alert("You don't have enough money");
+              } else {
+                alert("Your health is already full");
+              }
+            }}
+            className="buy-button"
+          >
+            Buy
+          </button>
+        </CardContent>
+      </Card>
+      <h3 className="store-headings">My Badges</h3>
+      <div className="badge-cards">
+        <Card className="badge-card">
+          <CardContent>
+            <img
+              src={props.images[0]}
+              style={{ height: "65px", width: "65px" }}
+            />
+            <p>Straw Badge</p>
+            <p>Acquired when you meet your budget 1 time in a year</p>
+          </CardContent>
+        </Card>
+        <Card className="badge-card">
+          <CardContent>
+            <img
+              src={props.images[1]}
+              style={{ height: "65px", width: "65px" }}
+            />
+            <p>Stick Badge</p>
+            <p>Acquired when you meet your budget 3 times in a year</p>
+          </CardContent>
+        </Card>
+        <Card className="badge-card">
+          <CardContent>
+            <img
+              src={props.images[2]}
+              style={{ height: "65px", width: "65px" }}
+            />
+            <p>{props.description}</p>
+          </CardContent>
+        </Card>
+        <Card className="badge-card">
+          <CardContent>
+            <img
+              src={props.images[3]}
+              style={{ height: "65px", width: "65px" }}
+            />
+            <p>{props.description}</p>
+          </CardContent>
+        </Card>
       </div>
       <Navbar location={history.location.pathname.slice(1)} />
     </div>
