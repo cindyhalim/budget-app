f1a9ad0c9f0326d4b9a234fae92c324b6b7b74b2
ahana <ahanaghosh94@gmail.com>
2019-11-04 11:23:32 -0500

    add style to store

--- budget-frontend/src/components/Store.js
+++ budget-frontend/src/components/Store.js
@@ -1,43 +1,54 @@
 import React from "react";
 import Navbar from "./Navbar";
 
+import StoreMallDirectoryIcon from "@material-ui/icons/StoreMallDirectory";
+
+import "../styles/Store.sass";
+
 export default function Store(props) {
-  console.log(props);
   props.checkLogInStatus();
   return (
     <div>
-      Store Your Coins:
-      <img src="coins.jpg" style={{ height: "30px", width: "30px" }} />
-      {props.coins}
-      <div>
-        Items To Purchase: Potion
-        <br></br>
-        <img src="potion.png" style={{ height: "30px", width: "30px" }} />
-        <br></br>
-        Cost: 20
-        <br></br>
-        Use it to heal 20 HP
-        <br></br>
-        <button
-          onClick={() => {
-            if (props.hp !== 100 && props.coins >= 20) {
-              props.subtractCoinsAddHP(-20, 20);
-            } else if (props.coins < 20) {
-              alert("You don't have enough money");
-            } else {
-              alert("Your health is already full");
-            }
-          }}
-        >
-          Buy
-        </button>
+      <div className="store-title">
+        <StoreMallDirectoryIcon className="store-icon" />
+        <div>Store</div>
       </div>
-      <div>
-        <p>
-          You have met your budget <strong>{props.budgetAchieved}</strong> times
-          this year
-        </p>
-        <p>See your badges below:</p>
+      <div className="entire-store">
+        <div className="inventory-card">
+          <div className="inventory-title">Inventory</div>
+          <div className="coins-inventory">
+            <div>Coins:</div>
+            <div>
+              <img src="coins.jpg" style={{ height: "30px", width: "30px" }} />
+              {props.coins}
+            </div>
+          </div>
+        </div>
+        <div className="market-card">
+          <div className="market-title">Market</div>
+          <img src="potion.png" style={{ height: "30px", width: "30px" }} />
+          Cost: 20 Use it to heal 20 HP
+          <button
+            onClick={() => {
+              if (props.hp !== 100 && props.coins >= 20) {
+                props.subtractCoinsAddHP(-20, 20);
+              } else if (props.coins < 20) {
+                alert("You don't have enough money");
+              } else {
+                alert("Your health is already full");
+              }
+            }}
+          >
+            Buy
+          </button>
+        </div>
+        <div>
+          <p>
+            You have met your budget <strong>{props.budgetAchieved}</strong>{" "}
+            times this year
+          </p>
+          <p>My Badges</p>
+        </div>
       </div>
       <Navbar />
     </div>
