f10658d2ff81f73607a276e7094bb04c529584ec
Cindy Halim <jcindyhalim@gmail.com>
2019-11-03 20:56:34 -0500

    Add margin bottom to analytics

--- budget-frontend/src/components/SingleTransaction.js
+++ budget-frontend/src/components/SingleTransaction.js
@@ -6,23 +6,21 @@ import "../styles/SingleTransaction.sass";
 
 const SingleTransaction = ({ amount, location, transactionDate }) => {
   return (
-    <div>
-      <Card elevation={0}>
-        <CardContent className="transaction-card" elevation={2}>
-          <h4>{location}</h4>
-          <div className="transaction-details">
-            <div className="transaction-labels">
-              <p>Amount:</p>
-              <p>Date:</p>
-            </div>
-            <div className="transaction-content">
-              <p>${Number(amount).toFixed(2)}</p>
-              <p>{moment(transactionDate).format("MMMM Do YYYY, h:mm:ss a")}</p>
-            </div>
+    <Card className="transaction" elevation={0}>
+      <CardContent className="transaction-card" elevation={2}>
+        <h4>{location}</h4>
+        <div className="transaction-details">
+          <div className="transaction-labels">
+            <p>Amount:</p>
+            <p>Date:</p>
           </div>
-        </CardContent>
-      </Card>
-    </div>
+          <div className="transaction-content">
+            <p>${Number(amount).toFixed(2)}</p>
+            <p>{moment(transactionDate).format("MMMM Do YYYY, h:mm:ss a")}</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
