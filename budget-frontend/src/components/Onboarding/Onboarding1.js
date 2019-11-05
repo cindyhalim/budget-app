4e2a88061aaa764daf62b9424b7406e8bc592046
ahana <ahanaghosh94@gmail.com>
2019-11-05 10:53:06 -0500

    Add style to onboarding page1

--- budget-frontend/src/components/Onboarding/Onboarding1.js
+++ budget-frontend/src/components/Onboarding/Onboarding1.js
@@ -5,9 +5,13 @@ const Onboarding1 = () => {
   return (
     <div>
       <h1 className="title">Welcome</h1>
-      <img src="pig.png" className="avatar"></img>
+      <img src="pig.png" className="avatar-pig"></img>
       <div>
-        Monthly budgeting app designed to help you save and track your spending.
+        <div>
+          Monthly budgeting app designed to help you save and track your
+          spending.
+        </div>
+        <div>Gain important insight and take control of your money</div>
       </div>
     </div>
   );
